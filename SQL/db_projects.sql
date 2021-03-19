d8bb8f5761ebd14e6caa0493c886bef1521a57b4
costales <costales.marcos@gmail.com>
2021-03-19 12:47:08 -0400

    Added mysql & dockerfile

---
+++ SQL/db_projects.sql
@@ -0,0 +1,3 @@
+create table projects(id int(11) not null, nombre varchar(256) not null, primary key(id));
+insert into projects(id,nombre) values (1,'marcos');
+insert into projects(id,nombre) values (2,'ruben');
