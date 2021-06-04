e82fbd46e56c9a0af26c9e9fd443b8b8c94e2519
Costales <costales.marcos@gmail.com>
2021-06-04 14:45:02 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,12 @@
 <?php
 print "Hola mundo! php is " . PHP_VERSION . "\n";
+
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+
+if (!mysql_connect($db_host, $db_user, $db_pass))
+    print "No se pudo establecer conexión a la base de datos";
+else
+    print "Conectado a BBDD";
 ?>
