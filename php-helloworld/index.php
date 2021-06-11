30915dcbe3675f018e36a2b058970d5b0e586b93
Costales <costales.marcos@gmail.com>
2021-06-11 17:19:04 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,13 +1,15 @@
 <?php
 print "Hello mundo! php is " . PHP_VERSION . "\n\n";
 
-echo "Conectando BBDD...\n\n";
+echo "Conectando BBDD...";
+print "\n";
 $db_handle = pg_connect("host=".$_ENV["HOST"]." dbname=".$_ENV["DATABASE"]." user=".$_ENV["USER"]." password=".$_ENV["PASSWORD"]);
 if ($db_handle) {
-echo 'CONECTADO\n\n';
+echo 'CONECTADO';
 } else {
-echo 'ERROR.\n\n';
+echo 'ERROR';
 }
+print "\n";
 pg_close($db_handle);
 echo "Conexión cerrada...\n";
 ?>
