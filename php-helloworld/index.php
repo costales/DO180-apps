6766e3774ad0b5d2259dff6094b333d00bdabb78
Costales <costales.marcos@gmail.com>
2021-06-11 13:45:06 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,10 +4,10 @@ print "Hello2 mundo! php is " . PHP_VERSION . "\n\n";
 echo "Conectando BBDD...\n\n";
 $db_handle = pg_connect("host=".$_ENV["HOST"]." dbname=".$_ENV["DATABASE"]." user=".$_ENV["USER"]." password=".$_ENV["PASSWORD"]);
 if ($db_handle) {
-echo 'Connection attempt succeeded.\\nn';
+echo 'Connection attempt succeeded.\n\n';
 } else {
-echo 'Connection attempt failed.\\nn';
+echo 'Connection attempt failed.\n\n';
 }
 pg_close($db_handle);
-echo "FIN BBDD...\n";
+echo "Conexión cerrada...\n";
 ?>
