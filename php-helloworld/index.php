ef9af847040cfd87f60e25cd2dda408ac106423f
Costales <costales.marcos@gmail.com>
2021-06-11 11:22:08 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,7 @@ print "Hello2 mundo! php is " . PHP_VERSION . "\n";
 
 echo 'Database user is: ' .$_ENV["MYSQL_USER"];
 
+echo "Conectando BBDD...";
 $db_handle = pg_connect("host=127.0.0.1 dbname=items user=user1 password=mypa55");
 if ($db_handle) {
 echo 'Connection attempt succeeded.';
@@ -10,4 +11,5 @@ echo 'Connection attempt succeeded.';
 echo 'Connection attempt failed.';
 }
 pg_close($db_handle);
+echo "FIN BBDD...";
 ?>
