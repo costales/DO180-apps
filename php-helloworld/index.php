ae6ad87abc0967bc2cf9ffc2856ddee2ab8114f3
Costales <costales.marcos@gmail.com>
2021-06-11 12:27:16 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,7 +2,7 @@
 print "Hello2 mundo! php is " . PHP_VERSION . "\n\n";
 
 echo "Conectando BBDD...\n\n";
-$db_handle = pg_connect("host=".$_ENV["POSTGREESQL_HOST"]." dbname=".$_ENV["POSTGREESQL_DATABASE"]." user=".$_ENV["POSTGREESQL_USER"]." password"=.$_ENV["POSTGREESQL_PASSWORD"]);
+$db_handle = pg_connect("host=".$_ENV["POSTGREESQL_HOST"]." dbname=".$_ENV["POSTGREESQL_DATABASE"]." user=".$_ENV["POSTGREESQL_USER"]." password=".$_ENV["POSTGREESQL_PASSWORD"]);
 if ($db_handle) {
 echo 'Connection attempt succeeded.\\nn';
 } else {
